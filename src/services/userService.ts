b4b802ac7f0ece9858c2e44fe0bb18ff79dfefb5
Ramon <ramonbraintrindade@gmail.com>
2024-07-19 00:31:58 -0300

    chore: Update npm dependencies and add bcrypt for password hashing

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,7 @@
+
 import connection from '../config/connection';
 import { User } from '../models/User';
+import bcrypt from 'bcrypt';
 
 export class UserService {
   static async getAllUsers(): Promise<User[]> {
@@ -16,10 +18,12 @@ export class UserService {
   static async createUser(user: User): Promise<User> {
     return new Promise((resolve, reject) => {
       const query =
-        'INSERT INTO users (name, email, contactNumber, password) VALUES (?, ?, ?, ?)';
+        "INSERT INTO users (name, email, contactNumber, password, isDeletable, status, role) VALUES (?, ?, ?, ?, 'true','true','user')";
+        const salt = 10;
+        const hasgPassword = bcrypt.hashSync(user.password, salt);
       connection.query(
         query,
-        [user.name, user.email, user.contactNumber, user.password],
+        [user.name, user.email, user.contactNumber, user.password = hasgPassword],
         (err: any, results: User) => {
           if (err) {
             reject(err);
