d33a5dee0707b6e65bbf8a9b07fcd291e1c753bc
ramontrndd <ramonbraintrindade@gmail.com>
2024-08-22 23:21:29 -0300

    chore: Update app.ts and server.ts for CORS support and port handling

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import router from './routes';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 
 app.use(express.json());
-app.use ('/user', router)
+app.use('/user', router);
 
 export default app;
-
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
-app.listen(port, () => {console.log(`Server is running on http://localhost:${port}`)});
-
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
